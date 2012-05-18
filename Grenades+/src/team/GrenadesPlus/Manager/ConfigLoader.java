126acfff3b0bf206c21a61da376b22c8d2e9bff9
unknown <robert.klassert@arcor.de>
2012-05-18 18:25:37 +0200

    Added loadRecipes method.
    Signed-off-by: unknown <robert.klassert@arcor.de>

--- Grenades+/src/team/GrenadesPlus/Manager/ConfigLoader.java
+++ Grenades+/src/team/GrenadesPlus/Manager/ConfigLoader.java
@@ -6,6 +6,9 @@ import java.util.List;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.inventory.ItemStack;
+import org.getspout.spoutapi.inventory.SpoutItemStack;
+import org.getspout.spoutapi.material.CustomBlock;
+import org.getspout.spoutapi.material.CustomItem;
 
 import team.GrenadesPlus.GrenadesPlus;
 import team.GrenadesPlus.Util.Util;
@@ -44,6 +47,41 @@ public class ConfigLoader {
 			FileManager.copy(GrenadesPlus.plugin.getResource("recipes.yml"), ConfigLoader.recipeFile);
 		if(FileManager.create(generalFile))
 			FileManager.copy(GrenadesPlus.plugin.getResource("general.yml"), ConfigLoader.generalFile);
+	}
+	
+	public static void loadRecipes(){
+		Object[] recipeKeys = recipeConfig.getKeys(false).toArray();
+		for(Object key : recipeKeys){
+			try{
+				YamlConfiguration defaultConfig = new YamlConfiguration();
+				defaultConfig.load(GrenadesPlus.plugin.getResource("recipes.yml"));
+				for(String node : defaultConfig.getConfigurationSection("Grenade").getKeys(false)){
+					if(recipeConfig.get(key+"."+node)==null){
+						Util.warn( "The node '"+node+"' in the recipe for "+key+" is missing or invalid!");
+					}
+				}
+				
+				Object cm = null;
+				if(Util.isGrenadesPlusMaterial(key.toString()))cm = Util.getGrenadesPlusMaterial(key.toString());
+				else throw new Exception(GrenadesPlus.PRE + " Recipe output not found: "+key+"! Skipping!");
+				int amount = recipeConfig.getInt(key.toString()+".amount");
+				SpoutItemStack result = null;
+				if(cm instanceof CustomItem){
+					CustomItem ci = (CustomItem)cm;
+					result = new SpoutItemStack(ci, amount);
+				}
+				else if(cm instanceof CustomBlock){
+					CustomBlock cb = (CustomBlock) cm;
+					result = new SpoutItemStack(cb, amount);
+				}
+				List<ItemStack> ingredients = ConfigParser.parseItems(recipeConfig.getString(key+".ingredients"));
+				team.GrenadesPlus.Manager.RecipeManager.RecipeType type = team.GrenadesPlus.Manager.RecipeManager.RecipeType.valueOf(recipeConfig.getString(key+".type").toUpperCase());
+				RecipeManager.addRecipe(type, ingredients, result);
+			}catch (Exception e) {
+				Util.warn("Config Error:" + e.getMessage());
+				Util.debug(e);
+			}
+		}
 	}
 
 	public static void loadGeneral() {
